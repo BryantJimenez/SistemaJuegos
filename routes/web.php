594f3efaf0375a7ddfc5eda7cc369b3e64d037e6
unknown <laurazzrangel760@gmail.com>
2019-12-15 18:20:00 -0400

    qlq

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ Auth::routes(['register' => false]);
 
 // ADMIN
 
-// Inicio
+// inicio
 Route::get('/', 'AdminController@index')->name('home');
 
-// Usuarios
+// usuarios
 Route::get('/usuarios', 'UserController@index')->name('usuarios.index');
 Route::get('/usuarios/registrar', 'UserController@create')->name('usuarios.create');
 Route::post('/usuarios', 'UserController@store')->name('usuarios.store');
@@ -29,16 +29,7 @@ Route::put('/usuarios/{slug}', 'UserController@update')->name('usuarios.update')
 Route::delete('/usuarios/{slug}', 'UserController@destroy')->name('usuarios.destroy');
 // Route::get('/perfil', 'UserController@profile')->name('usuarios.profile');
 
-// Jugadores
-Route::get('/jugadores', 'GamerController@index')->name('jugadores.index');
-Route::get('/jugadores/registrar', 'GamerController@create')->name('jugadores.create');
-Route::post('/jugadores', 'GamerController@store')->name('jugadores.store');
-Route::get('/jugadores/{slug}', 'GamerController@show')->name('jugadores.show');
-Route::get('/jugadores/{slug}/editar', 'GamerController@edit')->name('jugadores.edit');
-Route::put('/jugadores/{slug}', 'GamerController@update')->name('jugadores.update');
-Route::delete('/jugadores/{slug}', 'GamerController@destroy')->name('jugadores.destroy');
-
-// Torneos
+//Torneos
 Route::get('/torneos', 'TournamentController@index')->name('torneos.index');
 Route::get('/torneos/registrar', 'TournamentController@create')->name('torneos.create');
 Route::post('/torneos', 'TournamentController@store')->name('torneos.store');
@@ -46,6 +37,7 @@ Route::get('/torneos/{slug}', 'TournamentController@show')->name('torneos.show')
 Route::get('/torneos/{slug}/editar', 'TournamentController@edit')->name('torneos.edit');
 Route::put('/torneos/{slug}', 'TournamentController@update')->name('torneos.update');
 Route::delete('/torneos/{slug}', 'TournamentController@destroy')->name('torneos.destroy');
+
 
 //Clubes
 Route::get('/clubes', 'ClubController@index')->name('clubes.index');
@@ -60,9 +52,11 @@ Route::delete('/clubes/{slug}', 'ClubController@destroy')->name('clubes.destroy'
 
 //Club
 Route::get('/clubes', 'CouplesAssignamentController@index')->name('couples_assignament_list');
-Route::get('/clubes/registrar', 'CouplesAssignamentController@create')->name('couples_assignament.');
+Route::get('/clubes/registrar', 'CouplesAssignamentController@create')->name('couples_assignament');
 Route::post('/clubes', 'CouplesAssignamentController@store')->name('couples_assignament.store');
 Route::get('/clubes/{slug}', 'CouplesAssignamentController@show')->name('couples_assignament.show');
 Route::get('/clubes/{slug}/editar', 'CouplesAssignamentController@edit')->name('couples_assignament.edit');
 Route::put('/clubes/{slug}', 'CouplesAssignamentController@update')->name('couples_assignament.update');
 Route::delete('/clubes/{slug}', 'CouplesAssignamentController@destroy')->name('couples_assignament.destroy');
+
+
